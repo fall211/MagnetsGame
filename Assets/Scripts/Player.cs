4b95eeecd62f38ef2988efcbd79beedc41c9efa9
gahishi <133333699+gahishi@users.noreply.github.com>
2023-05-21 05:35:49 -0700

    breaking: try to fix machine-depedent behavior of player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,7 +8,7 @@ public class Player : MonoBehaviour
     // public float charge = 1f; 
     public Vector3 velosity; // initial velosity with direction specified by the user
     public Vector3 force; // current total force on the ball
-    const float STEP = 0.001f; // the amount forward per frame; should be const, 0.001 would fast but not that accurate, the numerical error
+    const float STEP = 0.03f; // the amount forward per frame; should be const, 0.001 would fast but not that accurate, the numerical error
 
     void totalForce(){
         force = new Vector3(0,0,0);
@@ -21,7 +22,9 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // try to force set fps
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
