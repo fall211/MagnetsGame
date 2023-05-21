60a446181b281b43c759556b40ab6cc06ae9e064
jssarai <jssarai@ucdavis.edu>
2023-05-20 22:59:53 -0700

    spike complete

--- Assets/Scripts/HitObstacle.cs
+++ Assets/Scripts/HitObstacle.cs
@@ -6,16 +6,29 @@ public class HitObstacle : MonoBehaviour
 {
     private Rigidbody2D rb;
     Vector2 lastVelocity;
+    public Sprite[] spriteArray;
+    public SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Start()
+    {
+        spriteRenderer.sprite = spriteArray[0];
+    }
+
     // Update is called once per frame
     void Update()
     {
         lastVelocity = rb.velocity;
+    }
+
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = spriteArray[1];
     }
 
     private void OnCollisionEnter2D(Collision2D obj) 
@@ -26,7 +39,8 @@ public class HitObstacle : MonoBehaviour
             rb.velocity = dir * Mathf.Max(speed, 0f);
         } else if (obj.gameObject.CompareTag("Spike"))
         {
-            Destroy(rb.gameObject);
+            ChangeSprite();
+            rb.velocity = 0*rb.velocity;
         }
 
     }
